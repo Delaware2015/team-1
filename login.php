0e106d3eed685c451f6ce0c613f23e61e0fdb1f3
trwha1 <trwha1@morgan.edu>
2015-10-31 11:11:19 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,11 @@
+<form action = "findstud.php" method="post" />
+<p>Name: <input type ="text" name="name" /></p>
+<p>Password: <input type ="password" name="password" /></p>
+<input type="submit" value="Login" /></br>
+</form>
+
+<form action = "studinfo.php" method="post" />
+<input type="submit" value="New User" /></br>
+</form>
+
+
