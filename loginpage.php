0728e090feb5c06161d316eac4fbbeea8f3f6fc2
trwha1 <trwha1@morgan.edu>
2015-10-31 11:11:41 -0400

    Create loginpage.php

---
+++ loginpage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML >
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Hello World</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</head>
+<a  class="btn btn-primary " href="hompage.php#">HOME</a>
+<body class="container"style="background-color:yellowgreen;color:Black;">
+<div class="jumbotron">
+<h1>The Children's Aid Society </h1>
+<p>(Single Sign-On)</p>
+
+<form action = "new.php" method="post" />
+<p>Name: <input type ="text" name="name" /></p>
+<p>Password: <input type ="password" name="password" /></p>
+<input type="submit" value="Login" /></br>
+</form>
+
+</body>
+</html>
