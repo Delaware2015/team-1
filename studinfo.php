6d9765e5509cffd1e39fcf8ffe4cc912b5cac413
trwha1 <trwha1@morgan.edu>
2015-10-31 11:13:15 -0400

    Create studinfo.php

---
+++ studinfo.php
@@ -0,0 +1,24 @@
+
+<form action = "studentdb.php" method="post" />
+
+<p>Name: <input type ="text" name="name" /></p>
+<p>Password: <input type ="password" name="password" /></p>
+<p>Distance in miles: <input type ="text" name="distance" /></p>
+<p>Year (2 or 4 years): <input type ="text" name="yr" /></p>
+<p>Area (Rural, Urban, Suburban) <input type ="text" name="area" /></p>
+<p>GPA : <input type ="text" name="gpa" /></p>
+<p>SAT scores: <input type ="text" name="satscores" /></p>
+<p>Dream Job: <input type ="text" name="dreamjob" /></p>
+
+<p>Major
+<select name="major" >
+<option value="1">Computer Science</option>
+<option value="2">Physics</option>
+<option value="3">business</option>
+
+</select>
+</p>
+<p>Budget: <input type ="text" name="cost" /></p>
+
+<input type="submit" value="Login" /></br>
+</form>
